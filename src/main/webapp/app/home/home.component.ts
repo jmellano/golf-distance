dda1873ae76d451c0cf7d0f6afcbf64f40de19b5
Julien Mellano <j.mellano@free.fr>
2017-10-21 18:47:46 +0200

    feat: pop-in ajout d'un coup
    - formulaire de saisie d'un nouveau coup
    - mise à jour des données

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,15 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
-import {JhiEventManager, JhiAlertService} from 'ng-jhipster';
+import {ModalDismissReasons, NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 
-import {CalibrationService} from '../entities/calibration/calibration.service';
-import {Calibration} from '../entities/calibration/calibration.model';
+import {JhiAlertService, JhiEventManager} from 'ng-jhipster';
 
 import {PlayerClubService} from '../entities/player-club/player-club.service';
+import {ShotService} from '../entities/shot/shot.service';
 import {PlayerClub} from '../entities/player-club/player-club.model';
 
-import {ResponseWrapper, Account, LoginModalService, Principal} from '../shared';
-import * as _ from 'lodash';
+import {Account, LoginModalService, Principal, ResponseWrapper} from '../shared';
+import {Shot} from '../entities/shot/shot.model';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector: 'jhi-home',
@@ -23,18 +23,33 @@ export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
     playerClubs: PlayerClub[];
+    playerId: number;
+    closeResult: string;
+    forceDisponible: any[];
+
+    isSaving: boolean;
+    shot: Shot;
+    modalAddShot: NgbModalRef;
 
     constructor(private principal: Principal,
                 private alertService: JhiAlertService,
                 private loginModalService: LoginModalService,
+                private modalService: NgbModal,
                 private eventManager: JhiEventManager,
-                private playerClubService: PlayerClubService) {
+                private playerClubService: PlayerClubService,
+                private shotService: ShotService) {
     }
 
     loadAll() {
         const self = this;
         this.principal.identity().then(function (res) {
-            self.playerClubService.queryForPlayer(res.playerId).subscribe(
+            self.playerId = res.playerId;
+            self.forceDisponible = [{id: '0.25', label: '25%'},
+                {id: '0.50', label: '50%'},
+                {id: '0.75', label: '75%'},
+                {id: '0.90', label: '90%'},
+                {id: '1.00', label: '100%'}];
+            self.playerClubService.queryForPlayer(self.playerId).subscribe(
                 (resPC: ResponseWrapper) => {
                     self.playerClubs = resPC.json;
                 },
@@ -43,14 +58,63 @@ export class HomeComponent implements OnInit {
         });
     }
 
+    open(content) {
+        this.modalAddShot = this.modalService.open(content);
+        this.modalAddShot.result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+
+    }
+
+    addShot() {
+        this.subscribeToSaveResponse(
+            this.shotService.create(this.shot));
+        this.registerAddShotSuccess();
+    }
+
+    private subscribeToSaveResponse(result: Observable<Shot>) {
+        result.subscribe((res: Shot) =>
+            this.onSaveSuccess(res), (res: Response) => this.onSaveError());
+    }
+
+    private onSaveSuccess(result: Shot) {
+        this.eventManager.broadcast({name: 'shotListModification', content: 'OK'});
+        this.isSaving = false;
+        this.modalAddShot.close();
+    }
+
+    private onSaveError() {
+        this.isSaving = false;
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
+
     ngOnInit() {
         this.loadAll();
-        console.log('lodash version:', _.VERSION);
+        this.shot = new Shot();
+        this.shot.playerClub = new PlayerClub();
 
         this.principal.identity().then((account) => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+    }
+
+    registerAddShotSuccess() {
+        this.eventManager.subscribe('shotListModification', (message) => {
+            console.log("Ok shot ajouté");
+            this.loadAll();
+        });
     }
 
     registerAuthenticationSuccess() {
